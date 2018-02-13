e5bd7c12bccc9c937714b1ea40ce1242f3daab95
git <2662908789@qq.com>
2018-02-13 21:24:35 +0800

    add Communication module

--- client/src/CommonLib/CommunicationClass.js
+++ client/src/CommonLib/CommunicationClass.js
@@ -3,7 +3,25 @@ import BaseClass from './BaseClass.js';
 class CommunicationClass extends BaseClass{
 	constructor() {
 		super();
-		console.log("CommunicationClass")
+		var that = this;
+		this.ws = new WebSocket('ws://localhost:3001') //'ws://localhost:3000'
+		this.ws.onopen = function () {
+			that.ws.send('ready');
+		};
+		
+		// 响应收到的消息:
+		this.ws.onmessage = function (message) {
+			console.log('[SERVER] reply:',message.data);
+		}
+		// 关闭的事件:
+		this.ws.onclose = function (message) {
+			// alert("close")
+			// console.log("close")
+		}
+	}
+	
+	getClientList(){
+		
 	}
 }
 
--- server/index.js
+++ server/index.js
@@ -1,13 +1,63 @@
 var path = require("path");
+const url = require('url');
 const express = require('express')
 const app = express()
 
+const WebSocket = require('ws');
+
+// 引用Server类:
+const WebSocketServer = WebSocket.Server;
+
+var wsclients = []
+
 var vis = require('./Router/vis');
-var serverport = 3000;
+var serverport = 3001;
 
 
 app.use('/', express.static('../client/build'));
 
 app.use('/vis',vis);
 
-app.listen(serverport, () => console.log('Example app listening on port '+serverport+'!'))
+let server = app.listen(
+	serverport, () => console.log('Example app listening on port '+serverport+'!')
+)
+
+
+// 创建WebSocketServer:
+let wss = new WebSocketServer({
+	server: server
+});
+
+wss.on('connection', function (ws, req) {
+	console.log("[SERVER] connection()");
+	const location = url.parse(req.url, true);
+	console.log(location);
+
+	ws.on('message', function (message) {
+		console.log('[SERVER] Received:',message);
+		ws.send('ok', (err) => {
+			if (err) {
+				console.log('[SERVER] error:',err);
+			}
+		});
+	})
+	ws.on('close', function (message) {
+		console.log('close');
+	})
+	ws.on('error', function (message) {
+		console.log('erro');
+	})
+});
+
+wss.broadcast = function (data) {
+	wss.clients.forEach(function (client) {
+		if (client.readyState === WebSocket.OPEN) {
+			client.send(data);
+		}
+	});
+};
+
+
+setInterval(function timeout() {
+	wss.broadcast("broadcast from server");
+}, 10000);
